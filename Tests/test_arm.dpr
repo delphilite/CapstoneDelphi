4feebfc72de8b7fa87eaea77acd9a56a7cec1ac6
lsuper <mapfile@gmail.com>
2024-05-29 19:20:32 +0800

    Add arm/detail test case

---
+++ Tests/test_arm.dpr
@@ -0,0 +1,255 @@
+﻿program test_arm;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils, Windows, test_utils, Capstone.Api, Capstone.Arm;
+
+procedure print_insn_detail(handle: csh; ins: Pcs_insn);
+var
+  arm: Pcs_arm;
+  i: Integer;
+  l: string;
+  regs_read, regs_write: cs_regs;
+  regs_read_count, regs_write_count: Byte;
+  op: Pcs_arm_op;
+begin
+  // detail can be NULL on "data" instruction if SKIPDATA option is turned ON
+  if ins.detail = nil then
+    Exit;
+
+  arm := @ins.detail.arm;
+
+  if arm.op_count > 0 then
+    WriteLn(Format(#9'op_count: %u', [arm.op_count]));
+
+  for i := 0 to arm.op_count - 1 do
+  begin
+    op := @arm.operands[i];
+    case Integer(op.&type) of
+      ARM_OP_REG:
+        WriteLn(Format(#9#9'operands[%d].type: REG = %s', [i, cs_reg_name(handle, op.detail.reg)]));
+      ARM_OP_IMM:
+        WriteLn(Format(#9#9'operands[%d].type: IMM = 0x%s', [i, format_string_hex(op.detail.imm, '%x')]));
+      ARM_OP_FP:
+{$IFDEF _KERNEL_MODE}
+        // Issue #681: Windows kernel does not support formatting float point
+        WriteLn(Format(#9#9'operands[%d].type: FP = <float_point_unsupported>', [i]));
+{$ELSE}
+        WriteLn(Format(#9#9'operands[%d].type: FP = %f', [i, op.detail.fp]));
+{$ENDIF}
+      ARM_OP_MEM_:
+        begin
+          WriteLn(Format(#9#9'operands[%d].type: MEM', [i]));
+          if op.detail.mem.base <> ARM_REG_INVALID then
+            WriteLn(Format(#9#9#9'operands[%d].mem.base: REG = %s', [i, cs_reg_name(handle, op.detail.mem.base)]));
+          if op.detail.mem.index <> ARM_REG_INVALID then
+            WriteLn(Format(#9#9#9'operands[%d].mem.index: REG = %s', [i, cs_reg_name(handle, op.detail.mem.index)]));
+          if op.detail.mem.scale <> 1 then
+            WriteLn(Format(#9#9#9'operands[%d].mem.scale: %u', [i, op.detail.mem.scale]));
+          if op.detail.mem.disp <> 0 then
+            WriteLn(Format(#9#9#9'operands[%d].mem.disp: 0x%s', [i, format_string_hex(op.detail.mem.disp, '%x')]));
+          if op.detail.mem.lshift <> 0 then
+            WriteLn(Format(#9#9#9'operands[%d].mem.lshift: 0x%s', [i, format_string_hex(op.detail.mem.lshift, '%x')]));
+        end;
+      ARM_OP_PIMM:
+        WriteLn(Format(#9#9'operands[%d].type: P-IMM = %u', [i, op.detail.imm]));
+      ARM_OP_CIMM:
+        WriteLn(Format(#9#9'operands[%d].type: C-IMM = %u', [i, op.detail.imm]));
+      ARM_OP_SETEND:
+        if op.detail.setend = ARM_SETEND_BE then
+          WriteLn(Format(#9#9'operands[%d].type: SETEND = %s', [i, 'be']))
+        else WriteLn(Format(#9#9'operands[%d].type: SETEND = %s', [i, 'le']));
+      ARM_OP_SYSREG:
+        WriteLn(Format(#9#9'operands[%d].type: SYSREG = %u', [i, op.detail.reg]));
+    end;
+
+    if op.neon_lane <> -1 then
+      WriteLn(Format(#9#9'operands[%d].neon_lane = %u', [i, op.neon_lane]));
+
+    case op.access of
+      CS_AC_READ:
+        WriteLn(Format(#9#9'operands[%d].access: READ', [i]));
+      CS_AC_WRITE:
+        WriteLn(Format(#9#9'operands[%d].access: WRITE', [i]));
+      CS_AC_READ or CS_AC_WRITE:
+        WriteLn(Format(#9#9'operands[%d].access: READ | WRITE', [i]));
+    end;
+
+    if (op.shift.&type <> ARM_SFT_INVALID) and (op.shift.value <> 0) then
+    begin
+      if op.shift.&type < ARM_SFT_ASR_REG then
+        // shift with constant value
+        WriteLn(Format(#9#9#9'Shift: %u = %u', [op.shift.&type, op.shift.value]))
+      else
+        // shift with register
+        WriteLn(Format(#9#9#9'Shift: %u = %s', [op.shift.&type, cs_reg_name(handle, op.shift.value)]));
+    end;
+
+    if op.vector_index <> -1 then
+      WriteLn(Format(#9#9'operands[%d].vector_index = %u', [i, op.vector_index]));
+
+    if op.subtracted then
+      WriteLn(#9#9'Subtracted: True');
+  end;
+
+  if (arm.cc <> ARM_CC_AL) and (arm.cc <> ARM_CC_INVALID) then
+    WriteLn(Format(#9'Code condition: %u', [arm.cc]));
+
+  if arm.update_flags then
+    WriteLn(#9'Update-flags: True');
+
+  if arm.writeback then
+    WriteLn(#9'Write-back: True');
+
+  if arm.cps_mode <> 0 then
+    WriteLn(Format(#9'CPSI-mode: %u', [arm.cps_mode]));
+
+  if arm.cps_flag <> 0 then
+    WriteLn(Format(#9'CPSI-flag: %u', [arm.cps_flag]));
+
+  if arm.vector_data <> 0 then
+    WriteLn(Format(#9'Vector-data: %u', [arm.vector_data]));
+
+  if arm.vector_size <> 0 then
+    WriteLn(Format(#9'Vector-size: %u', [arm.vector_size]));
+
+  if arm.usermode then
+    WriteLn(#9'User-mode: True');
+
+  if arm.mem_barrier <> 0 then
+    WriteLn(Format(#9'Memory-barrier: %u', [arm.mem_barrier]));
+
+  // Print out all registers accessed by this instruction (either implicit or explicit)
+  if cs_regs_access(handle, ins, regs_read, @regs_read_count, regs_write, @regs_write_count) = 0 then
+  begin
+    if regs_read_count > 0 then
+    begin
+      l := #9'Registers read:';
+      for i := 0 to regs_read_count - 1 do
+      begin
+        l := l + ' ' + string(cs_reg_name(handle, regs_read[i]));
+      end;
+      WriteLn(l);
+    end;
+
+    if regs_write_count > 0 then
+    begin
+      l := #9'Registers modified:';
+      for i := 0 to regs_write_count - 1 do
+      begin
+        l := l + ' ' + string(cs_reg_name(handle, regs_write[i]));
+      end;
+      WriteLn(l);
+    end;
+  end;
+
+  WriteLn('');
+end;
+
+procedure test;
+const
+  ARM_CODE: array[0..51] of Byte = (
+    $86, $48, $60, $F4, $4D, $0F, $E2, $F4, $ED, $FF, $FF, $EB, $04, $E0, $2D, $E5,
+    $00, $00, $00, $00, $E0, $83, $22, $E5, $F1, $02, $03, $0E, $00, $00, $A0, $E3,
+    $02, $30, $C1, $E7, $00, $00, $53, $E3, $00, $02, $01, $F1, $05, $40, $D0, $E8,
+    $F4, $80, $00, $00
+  );
+  ARM_CODE2: array[0..19] of Byte = (
+    $D1, $E8, $00, $F0, $F0, $24, $04, $07, $1F, $3C, $F2, $C0, $00, $00, $4F, $F0,
+    $00, $01, $46, $6C
+  );
+  THUMB_CODE: array[0..31] of Byte = (
+    $60, $F9, $1F, $04, $E0, $F9, $4F, $07, $70, $47, $00, $F0, $10, $E8, $EB, $46,
+    $83, $B0, $C9, $68, $1F, $B1, $30, $BF, $AF, $F3, $20, $84, $52, $F8, $23, $F0
+  );
+  THUMB_CODE2: array[0..35] of Byte = (
+    $4F, $F0, $00, $01, $BD, $E8, $00, $88, $D1, $E8, $00, $F0, $18, $BF, $AD, $BF,
+    $F3, $FF, $0B, $0C, $86, $F3, $00, $89, $80, $F3, $00, $8C, $4F, $FA, $99, $F6,
+    $D0, $FF, $A2, $01
+  );
+  THUMB_MCLASS: array[0..3] of Byte = (
+    $EF, $F3, $02, $80
+  );
+  ARMV8: array[0..11] of Byte = (
+    $E0, $3B, $B2, $EE, $42, $00, $01, $E1, $51, $F0, $7F, $F5
+  );
+const
+  Platforms: array [0..5] of TPlatform = (
+    (arch: CS_ARCH_ARM; mode: CS_MODE_ARM; code: @ARM_CODE; size: SizeOf(ARM_CODE); comment: 'ARM'; syntax: 0),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB; code: @THUMB_CODE; size: SizeOf(THUMB_CODE); comment: 'Thumb'; syntax: 0),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB; code: @ARM_CODE2; size: SizeOf(ARM_CODE2); comment: 'Thumb-mixed'; syntax: 0),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB; code: @THUMB_CODE2; size: SizeOf(THUMB_CODE2); comment: 'Thumb-2 & register named with numbers'; syntax: CS_OPT_SYNTAX_NOREGNAME),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB + CS_MODE_MCLASS; code: @THUMB_MCLASS; size: SizeOf(THUMB_MCLASS); comment: 'Thumb-MClass'; syntax: 0),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_ARM + CS_MODE_V8; code: @ARMV8; size: SizeOf(ARMV8); comment: 'Arm-V8'; syntax: 0)
+  );
+var
+  handle: csh;
+  address: UInt64;
+  insn, item: Pcs_insn;
+  i, j: Integer;
+  count: Integer;
+  l: string;
+  err: cs_err;
+begin
+  for i := Low(Platforms) to High(Platforms) do
+  begin
+    address := $80001000;
+    err := cs_open(Platforms[i].arch, Platforms[i].mode, handle);
+    if err <> CS_ERR_OK then
+    begin
+      WriteLn('Failed on cs_open() with error returned: ', Ord(err));
+      Abort;
+    end;
+
+    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+
+    if Platforms[i].syntax <> 0 then
+      cs_option(handle, CS_OPT_SYNTAX, Platforms[i].syntax);
+
+    count := cs_disasm(handle, Platforms[i].code, Platforms[i].size, address, 0, insn);
+    if count > 0 then
+    try
+      WriteLn('****************');
+      WriteLn('Platform: ', Platforms[i].comment);
+      print_string_hex('Code: ', Platforms[i].code, Platforms[i].size);
+      WriteLn('Disasm:');
+
+      item := insn;
+      for j := 0 to count - 1 do
+      begin
+        l := '0x' + format_string_hex(item.address);
+        WriteLn(Format('%s:'#9'%s'#9'%s', [l, item.mnemonic, item.op_str]));
+        print_insn_detail(handle, item);
+        if j < count - 1 then
+          Inc(item);
+      end;
+      l := '0x' + format_string_hex(item.address + item.size, '%.2x') + ':';
+      WriteLn(l);
+    finally
+      // free memory allocated by cs_disasm()
+      cs_free(insn, count);
+    end
+    else begin
+      WriteLn('****************');
+      WriteLn('Platform: ', Platforms[i].comment);
+      print_string_hex('Code: ', Platforms[i].code, Platforms[i].size);
+      WriteLn('ERROR: Failed to disasm given code!');
+      Abort;
+    end;
+
+    WriteLn('');
+
+    cs_close(handle);
+  end;
+end;
+
+begin
+  try
+    Test;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
+
---
+++ Tests/test_detail.dpr
@@ -0,0 +1,253 @@
+program test_detail;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils, Windows, test_utils, Capstone.Api;
+
+procedure Test;
+const
+{$IFDEF CAPSTONE_HAS_X86}
+  X86_CODE16: array[0..11] of Byte = (
+    $8D, $4C, $32, $08, $01, $D8, $81, $C6, $34, $12, $00, $00
+  );
+  X86_CODE32: array[0..11] of Byte = (
+    $8D, $4C, $32, $08, $01, $D8, $81, $C6, $34, $12, $00, $00
+  );
+  X86_CODE64: array[0..7] of Byte = (
+    $55, $48, $8B, $05, $B8, $13, $00, $00);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_ARM}
+  ARM_CODE: array[0..31] of Byte = (
+    $ED, $FF, $FF, $EB, $04, $E0, $2D, $E5, $00, $00, $00, $00, $E0, $83, $22, $E5,
+    $F1, $02, $03, $0E, $00, $00, $A0, $E3, $02, $30, $C1, $E7, $00, $00, $53, $E3
+  );
+  ARM_CODE2: array[0..15] of Byte = (
+    $10, $F1, $10, $E7, $11, $F2, $31, $E7, $DC, $A1, $2E, $F3, $E8, $4E, $62, $F3
+  );
+  THUMB_CODE: array[0..7] of Byte = (
+    $70, $47, $EB, $46, $83, $B0, $C9, $68
+  );
+  THUMB_CODE2: array[0..11] of Byte = (
+    $4F, $F0, $00, $01, $BD, $E8, $00, $88, $D1, $E8, $00, $F0
+  );
+  THUMB_MCLASS: array[0..3] of Byte = (
+    $EF, $F3, $02, $80
+  );
+  ARMV8: array[0..11] of Byte = (
+    $E0, $3B, $B2, $EE, $42, $00, $01, $E1, $51, $F0, $7F, $F5);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_MIPS}
+  MIPS_CODE: array[0..23] of Byte = (
+    $0C, $10, $00, $97, $00, $00, $00, $00, $24, $02, $00, $0C, $8F, $A2, $00, $00,
+    $34, $21, $34, $56, $00, $80, $04, $08
+  );
+  MIPS_CODE2: array[0..7] of Byte = (
+    $56, $34, $21, $34, $C2, $17, $01, $00
+  );
+  MIPS_32R6M: array[0..15] of Byte = (
+    $00, $07, $00, $07, $00, $11, $93, $7C, $01, $8C, $8B, $7C, $00, $C7, $48, $D0
+  );
+  MIPS_32R6: array[0..7] of Byte = (
+    $EC, $80, $00, $19, $7C, $43, $22, $A0);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_ARM64}
+  ARM64_CODE: array[0..67] of Byte = (
+    $09, $00, $38, $D5, $BF, $40, $00, $D5, $0C, $05, $13, $D5, $20, $50, $02, $0E,
+    $20, $E4, $3D, $0F, $00, $18, $A0, $5F, $A2, $00, $AE, $9E, $9F, $37, $03, $D5,
+    $BF, $33, $03, $D5, $DF, $3F, $03, $D5, $21, $7C, $02, $9B, $21, $7C, $00, $53,
+    $00, $40, $21, $4B, $E1, $0B, $40, $B9, $20, $04, $81, $DA, $20, $08, $02, $8B,
+    $10, $5B, $E8, $3C);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_POWERPC}
+  PPC_CODE: array[0..43] of Byte = (
+    $80, $20, $00, $00, $80, $3F, $00, $00, $10, $43, $23, $0E, $D0, $44, $00, $80,
+    $4C, $43, $22, $02, $2D, $03, $00, $80, $7C, $43, $20, $14, $7C, $43, $20, $93,
+    $4F, $20, $00, $21, $4C, $C8, $00, $21, $40, $82, $00, $14
+  );
+  PPC_CODE2: array[0..11] of Byte = (
+    $10, $60, $2A, $10, $10, $64, $28, $88, $7C, $4A, $5D, $0F);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_SPARC}
+  SPARC_CODE: array[0..63] of Byte = (
+    $80, $A0, $40, $02, $85, $C2, $60, $08, $85, $E8, $20, $01, $81, $E8, $00, $00,
+    $90, $10, $20, $01, $D5, $F6, $10, $16, $21, $00, $00, $0A, $86, $00, $40, $02,
+    $01, $00, $00, $00, $12, $BF, $FF, $FF, $10, $BF, $FF, $FF, $A0, $02, $00, $09,
+    $0D, $BF, $FF, $FF, $D4, $20, $60, $00, $D4, $4E, $00, $16, $2A, $C2, $80, $03
+  );
+  SPARCV9_CODE: array[0..15] of Byte = (
+    $81, $A8, $0A, $24, $89, $A0, $10, $20, $89, $A0, $1A, $60, $89, $A0, $00, $E0);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_SYSZ}
+  SYSZ_CODE: array[0..39] of Byte = (
+    $ED, $00, $00, $00, $00, $1A, $5A, $0F, $1F, $FF, $C2, $09, $80, $00, $00, $00,
+    $07, $F7, $EB, $2A, $FF, $FF, $7F, $57, $E3, $01, $FF, $FF, $7F, $57, $EB, $00,
+    $F0, $00, $00, $24, $B2, $4F, $00, $78);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_XCORE}
+  XCORE_CODE: array[0..27] of Byte = (
+    $FE, $0F, $FE, $17, $13, $17, $C6, $FE, $EC, $17, $97, $F8, $EC, $4F, $1F, $FD,
+    $EC, $37, $07, $F2, $45, $5B, $F9, $FA, $02, $06, $1B, $10);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_M68K}
+  M68K_CODE: array[0..41] of Byte = (
+    $D4, $40, $87, $5A, $4E, $71, $02, $B4, $C0, $DE, $C0, $DE, $5C, $00, $1D, $80,
+    $71, $12, $01, $23, $F2, $3C, $44, $22, $40, $49, $0E, $56, $54, $C5, $F2, $3C,
+    $44, $00, $44, $7A, $00, $00, $F2, $00, $0A, $28);
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_M680X}
+  M680X_CODE: array[0..34] of Byte = (
+    $06, $10, $19, $1A, $55, $1E, $01, $23, $E9, $31, $06, $34, $55, $A6, $81, $A7,
+    $89, $7F, $FF, $A6, $9D, $10, $00, $A7, $91, $A6, $9F, $10, $00, $11, $AC, $99,
+    $10, $00, $39
+  );
+{$ENDIF}
+const
+  Platforms: array[0..22] of TPlatform = (
+{$IFDEF CAPSTONE_HAS_X86}
+    (arch: CS_ARCH_X86; mode: CS_MODE_16; code: @X86_CODE16; size: SizeOf(X86_CODE16); comment: 'X86 16bit (Intel syntax)'),
+    (arch: CS_ARCH_X86; mode: CS_MODE_32; code: @X86_CODE32; size: SizeOf(X86_CODE32); comment: 'X86 32bit (ATT syntax)'; opt_type: CS_OPT_SYNTAX; opt_value: CS_OPT_SYNTAX_ATT),
+    (arch: CS_ARCH_X86; mode: CS_MODE_32; code: @X86_CODE32; size: SizeOf(X86_CODE32); comment: 'X86 32 (Intel syntax)'),
+    (arch: CS_ARCH_X86; mode: CS_MODE_64; code: @X86_CODE64; size: SizeOf(X86_CODE64); comment: 'X86 64 (Intel syntax)'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_ARM}
+    (arch: CS_ARCH_ARM; mode: CS_MODE_ARM; code: @ARM_CODE; size: SizeOf(ARM_CODE); comment: 'ARM'),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB; code: @THUMB_CODE2; size: SizeOf(THUMB_CODE2); comment: 'THUMB-2'),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_ARM; code: @ARM_CODE2; size: SizeOf(ARM_CODE2); comment: 'ARM: Cortex-A15 + NEON'),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB; code: @THUMB_CODE; size: SizeOf(THUMB_CODE); comment: 'THUMB'),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_THUMB + CS_MODE_MCLASS; code: @THUMB_MCLASS; size: SizeOf(THUMB_MCLASS); comment: 'Thumb-MClass'),
+    (arch: CS_ARCH_ARM; mode: CS_MODE_ARM + CS_MODE_V8; code: @ARMV8; size: SizeOf(ARMV8); comment: 'Arm-V8'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_MIPS}
+    (arch: CS_ARCH_MIPS; mode: CS_MODE_MIPS32 + CS_MODE_BIG_ENDIAN; code: @MIPS_CODE; size: SizeOf(MIPS_CODE); comment: 'MIPS-32 (Big-endian)'),
+    (arch: CS_ARCH_MIPS; mode: CS_MODE_MIPS64 + CS_MODE_LITTLE_ENDIAN; code: @MIPS_CODE2; size: SizeOf(MIPS_CODE2); comment: 'MIPS-64-EL (Little-endian)'),
+    (arch: CS_ARCH_MIPS; mode: CS_MODE_MIPS32R6 + CS_MODE_MICRO + CS_MODE_BIG_ENDIAN; code: @MIPS_32R6M; size: SizeOf(MIPS_32R6M); comment: 'MIPS-32R6 | Micro (Big-endian)'),
+    (arch: CS_ARCH_MIPS; mode: CS_MODE_MIPS32R6 + CS_MODE_BIG_ENDIAN; code: @MIPS_32R6; size: SizeOf(MIPS_32R6); comment: 'MIPS-32R6 (Big-endian)'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_ARM64}
+    (arch: CS_ARCH_ARM64; mode: CS_MODE_ARM; code: @ARM64_CODE; size: SizeOf(ARM64_CODE); comment: 'ARM-64'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_POWERPC}
+    (arch: CS_ARCH_PPC; mode: CS_MODE_BIG_ENDIAN; code: @PPC_CODE; size: SizeOf(PPC_CODE); comment: 'PPC-64'),
+    (arch: CS_ARCH_PPC; mode: CS_MODE_BIG_ENDIAN + CS_MODE_QPX; code: @PPC_CODE2; size: SizeOf(PPC_CODE2); comment: 'PPC-64 + QPX'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_SPARC}
+    (arch: CS_ARCH_SPARC; mode: CS_MODE_BIG_ENDIAN; code: @SPARC_CODE; size: SizeOf(SPARC_CODE); comment: 'Sparc'),
+    (arch: CS_ARCH_SPARC; mode: CS_MODE_BIG_ENDIAN + CS_MODE_V9; code: @SPARCV9_CODE; size: SizeOf(SPARCV9_CODE); comment: 'SparcV9'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_SYSZ}
+    (arch: CS_ARCH_SYSZ; mode: 0; code: @SYSZ_CODE; size: SizeOf(SYSZ_CODE); comment: 'SystemZ'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_XCORE}
+    (arch: CS_ARCH_XCORE; mode: 0; code: @XCORE_CODE; size: SizeOf(XCORE_CODE); comment: 'XCore'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_M68K}
+    (arch: CS_ARCH_M68K; mode: CS_MODE_BIG_ENDIAN + CS_MODE_M68K_040; code: @M68K_CODE; size: SizeOf(M68K_CODE); comment: 'M68K'),
+{$ENDIF}
+{$IFDEF CAPSTONE_HAS_M680X}
+    (arch: CS_ARCH_M680X; mode: CS_MODE_M680X_6809; code: @M680X_CODE; size: SizeOf(M680X_CODE); comment: 'M680X_M6809')
+{$ENDIF}
+  );
+var
+  handle: csh;
+  address: UInt64;
+  insn, item: Pcs_insn;
+  detail: Pcs_detail;
+  i, j, n: Integer;
+  l: string;
+  count: Integer;
+  err: cs_err;
+begin
+  for i := Low(Platforms) to High(Platforms) do
+  begin
+    Writeln('****************');
+    Writeln('Platform: ', Platforms[i].comment);
+
+    err := cs_open(Platforms[i].arch, Platforms[i].mode, handle);
+    if err <> CS_ERR_OK then
+    begin
+      Writeln('Failed on cs_open() with error returned: ', Err);
+      Continue;
+    end;
+
+    if Platforms[I].opt_type <> 0 then
+      cs_option(handle, Platforms[i].opt_type, Platforms[i].opt_value);
+
+    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+
+    address := $1000;
+    count := cs_disasm(handle, Platforms[i].code, Platforms[i].size, address, 0, insn);
+    if count > 0 then
+    begin
+      print_string_hex('Code: ', Platforms[i].code, Platforms[i].size);
+      Writeln('Disasm:');
+      item := insn;
+      for j := 0 to count - 1 do
+      begin
+        l := '0x' + format_string_hex(item.address, '%.4x');
+        l := Format('%s:'#9'%s'#9#9'%s // insn-ID: %d, insn-mnem: %s', [l, item.mnemonic, item.op_str, item.Id, cs_insn_name(handle, item.Id)]);
+        WriteLn(l);
+
+        detail := item.Detail;
+
+        if detail.regs_read_count > 0 then
+        begin
+          l := #9'Implicit registers read: ';
+          for n := 0 to detail.regs_read_count - 1 do
+          begin
+            l := l + string(cs_reg_name(handle, detail.regs_read[n])) + ' ';
+          end;
+          Writeln(l);
+        end;
+
+        if detail.regs_write_count > 0 then
+        begin
+          l := #9'Implicit registers modified: ';
+          for n := 0 to detail.regs_write_count - 1 do
+          begin
+            l := l + string(cs_reg_name(handle, detail.regs_write[n])) + ' ';
+          end;
+          Writeln(l);
+        end;
+
+        if detail.groups_count > 0 then
+        begin
+          l := #9'This instruction belongs to groups: ';
+          for n := 0 to detail.groups_count - 1 do
+          begin
+            l := l + string(cs_group_name(handle, detail.groups[n])) + ' ';
+          end;
+          Writeln(l);
+        end;
+
+        if j < count - 1 then
+          Inc(item);
+      end;
+
+      // print out the next offset, after the last insn
+      l := '0x' + format_string_hex(item.address + item.size, '%.4x') + ':';
+      Writeln(l);
+
+      // free memory allocated by cs_disasm()
+      cs_free(insn, count);
+    end
+    else begin
+      Writeln('****************');
+      Writeln('Platform: ', Platforms[i].Comment);
+      Writeln('ERROR: Failed to disasm given code!');
+      Continue;
+    end;
+
+    WriteLn('');
+    cs_close(Handle);
+  end;
+end;
+
+begin
+  try
+    Test;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
